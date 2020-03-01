4642ca902cbc08ab83f7302d1919b22b1b639019
Alessandro Ribeiro <aribeiro@avenuecode.com>
2020-03-01 03:01:01 -0300

    Updates

---
+++ precache-manifest.6d9ff742b9923d1914ab795c86386c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caee11e90e872b5c25180c9f326cde7",
+    "url": "/world-cities-nav/index.html"
+  },
+  {
+    "revision": "de5f1917026d45670e81",
+    "url": "/world-cities-nav/static/css/main.9cc0e51d.chunk.css"
+  },
+  {
+    "revision": "7db2f7af26e3ab89db34",
+    "url": "/world-cities-nav/static/js/2.f1dd7609.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/world-cities-nav/static/js/2.f1dd7609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5f1917026d45670e81",
+    "url": "/world-cities-nav/static/js/main.fcca108e.chunk.js"
+  },
+  {
+    "revision": "c6f4154259dbb5b98efa",
+    "url": "/world-cities-nav/static/js/runtime-main.40b32c5b.js"
+  }
+]);
